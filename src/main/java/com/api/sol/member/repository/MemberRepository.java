5313653cab642095dc1f85cb533dbf5fd01cf370
hershyYJ <juj0478@naver.com>
2024-10-08 16:54:42 +0900

    [feature/#2] Member repository 구현

---
+++ src/main/java/com/api/sol/member/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.api.sol.member.repository;
+
+import com.api.sol.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByClientId(String clientId);
+
+}
